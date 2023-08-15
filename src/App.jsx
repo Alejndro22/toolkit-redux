518d1ed2032c051f52e821fe0c1d228334f58446
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-08-14 23:59:03 -0600

    Using values form store and dispatching actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment } from './store/slices/counter';
+
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  // We can read data from the store with useSelector, and dispatch actions using useDispatch
+  const { counter } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <>
@@ -16,10 +20,14 @@ function App() {
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>count is {counter}</h1>
       <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button
+          onClick={() => {
+            dispatch(increment());
+          }}
+        >
+          Increment
         </button>
       </div>
     </>
