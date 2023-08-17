804c67834b0231987b534b933489a00a903973bd
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-08-16 23:14:53 -0600

    Consuming todos API with custom hook
    In this commit i configured the store to access the todosApi.reducer, and then display results in <ToDoApp />

--- src/ToDoApp.jsx
+++ src/ToDoApp.jsx
@@ -1,10 +1,24 @@
+import { useGetTodosQuery } from './store/apis/todos/todosAPI';
+
 export const ToDoApp = () => {
+  const { data: todos = [], isLoading } = useGetTodosQuery();
+
   return (
     <>
       <h1>Todos - RTKQuery</h1>
       <hr />
+      <h4>isLoading: {isLoading ? 'True' : 'False'}</h4>
 
       <pre>...</pre>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <strong>{todo.completed ? 'DONE: ' : 'Pending: '}</strong>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
 
       <button>Next To Do</button>
     </>
