56bad02ffc626eae1d5d9009fdc428dabb895d3c
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-08-16 22:17:41 -0600

    Introduction to basic usage for RTK Query

---
+++ src/ToDoApp.jsx
@@ -0,0 +1,12 @@
+export const ToDoApp = () => {
+  return (
+    <>
+      <h1>Todos - RTKQuery</h1>
+      <hr />
+
+      <pre>...</pre>
+
+      <button>Next To Do</button>
+    </>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client';
 // import App from './App.jsx';
 import { store } from './store';
 import { Provider } from 'react-redux';
+// import { PokemonApp } from './PokemonApp.jsx';
+import { ToDoApp } from './ToDoApp';
 import './index.css';
-import { PokemonApp } from './PokemonApp.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <App /> */}
-      <PokemonApp />
+      {/* <PokemonApp /> */}
+      <ToDoApp />
     </Provider>
   </React.StrictMode>
 );
