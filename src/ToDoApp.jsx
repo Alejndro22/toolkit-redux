6040d7836c2250b4e23f913bbee650514d29c5fa
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-08-16 23:31:55 -0600

    Understanding RTK Query benefits of caching
    This is an initial feature of great stuff about RTK Query, but this section was only an introduction. This test was with test/:id endpoint

--- src/ToDoApp.jsx
+++ src/ToDoApp.jsx
@@ -1,7 +1,22 @@
-import { useGetTodosQuery } from './store/apis/todos/todosAPI';
+import { useState } from 'react';
+import {
+  useGetTodoByIdQuery,
+  // useGetTodosQuery
+} from './store/apis/todos/todosAPI';
 
 export const ToDoApp = () => {
-  const { data: todos = [], isLoading } = useGetTodosQuery();
+  const [todoId, setTodoId] = useState(1);
+  // const { data: todos = [], isLoading } = useGetTodosQuery();
+  const { data: todo = [], isLoading } = useGetTodoByIdQuery(todoId);
+
+  const nextTodo = () => {
+    setTodoId((value) => value + 1);
+  };
+
+  const prevTodo = () => {
+    if (todoId <= 1) return;
+    setTodoId((value) => value - 1);
+  };
 
   return (
     <>
@@ -9,18 +24,20 @@ export const ToDoApp = () => {
       <hr />
       <h4>isLoading: {isLoading ? 'True' : 'False'}</h4>
 
-      <pre>...</pre>
+      <pre>{JSON.stringify(todo)}</pre>
 
-      <ul>
+      <button onClick={prevTodo}>Prev To Do</button>
+
+      <button onClick={nextTodo}>Next To Do</button>
+
+      {/* <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
             <strong>{todo.completed ? 'DONE: ' : 'Pending: '}</strong>
             {todo.title}
           </li>
         ))}
-      </ul>
-
-      <button>Next To Do</button>
+      </ul> */}
     </>
   );
 };
